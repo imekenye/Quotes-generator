d1b1e583dd488f77c79b755f126e9619f6913029
imekenye <ismekenye@gmail.com>
2019-03-23 12:34:54 -0700

    Add downvote component

---
+++ src/app/downvote/downvote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-downvote',
+  templateUrl: './downvote.component.html',
+  styleUrls: ['./downvote.component.scss']
+})
+export class DownvoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
