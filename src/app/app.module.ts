fd7cf0a4b492c93652786ee5d176555685f5384e
imekenye <ismekenye@gmail.com>
2019-03-23 12:32:37 -0700

    Add initial app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { QuoteFormComponent } from "./quote-form/quote-form.component";
+import { QuoteDetailsComponent } from "./quote-details/quote-details.component";
+import { UpvoteComponent } from "./upvote/upvote.component";
+import { QuoteComponent } from "./quote/quote.component";
+import { DateCountPipe } from './date-count.pipe';
+import { DownvoteComponent } from './downvote/downvote.component';
+import { FormsModule } from '@angular/forms';
+// import {AngularFontAwesomeModule} from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    QuoteFormComponent,
+    QuoteDetailsComponent,
+    UpvoteComponent,
+    QuoteComponent,
+    DateCountPipe,
+    DownvoteComponent,
+    // AngularFontAwesomeModule,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, 
+  FormsModule,
+  AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
