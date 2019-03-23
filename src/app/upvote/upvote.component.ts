888006376dffb2be6a7bfb993b24b7966e64f267
imekenye <ismekenye@gmail.com>
2019-03-23 12:48:10 -0700

    Add upvote component

---
+++ src/app/upvote/upvote.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-upvote",
+  templateUrl: "./upvote.component.html",
+  styleUrls: ["./upvote.component.scss"]
+})
+export class UpvoteComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
